7ecce4a8c19bfdeeca18a02835fbc9d8c41f5625
Jerry Towler <jtowler@swri.org>
2015-12-15 18:14:53 -0600

    Solve day 4, part 2

---
+++ 4/part2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import hashlib
+
+
+def hash_with_salt(string, salt):
+    return hashlib.md5(str(string) + str(salt)).hexdigest()
+
+with open('input', 'r') as f:
+    # Strip the trailing newline
+    data = f.read()[:-1]
+
+prefix = '000000'
+
+cur_salt = 0
+while True:
+    cur_hash = hash_with_salt(data, cur_salt)
+
+    if cur_hash.startswith(prefix):
+        print '{} with salt {} produces hash {}'.format(data,
+                                                        cur_salt,
+                                                        cur_hash)
+        break
+
+    cur_salt = cur_salt + 1
+
+    if cur_salt % 10000 == 0:
+        print 'Processing salt {}'.format(cur_salt)
