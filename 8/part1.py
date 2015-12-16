d3f3e54e242501fd6728aed66b5453b12faf35c5
Jerry Towler <jatowler@gmail.com>
2015-12-16 01:28:50 -0600

    Clarify decoding

--- 8/part1.py
+++ 8/part1.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 
 
-def count_code_chars(string):
-  return len(string)
-
-
-def count_string_chars(string):
-  length = len(string)
-  print 'Processing string {} ({})'.format(string, length)
+def decode(string):
+  print 'Decoding string {}'.format(string)
 
   # Strip the start and end quotes
   string = string[1:-1]
-  length = len(string)
-  print '  Strip quotes: {} ({})'.format(string, length)
+  print '  Strip quotes: {}'.format(string)
 
   # Replace char codes
   def replace_char_code(matchobject):
@@ -24,20 +18,17 @@ def count_string_chars(string):
   import re
   char_code = re.compile(r'\\x(..)')
   string = re.sub(char_code, replace_char_code, string)
-  length = len(string)
-  print '  Replace \\x: {} ({})'.format(string, length)
+  print '  Replace \\x: {}'.format(string)
 
   # Replace quotes
   string = string.replace('\\"', '"')
-  length = len(string)
-  print '  Replace \\": {} ({})'.format(string, length)
+  print '  Replace \\": {}'.format(string)
 
   # Replace backslashes
   string = string.replace('\\\\', '\\')
-  length = len(string)
-  print '  Replace \\\\: {} ({})'.format(string, length)
+  print '  Replace \\\\: {}'.format(string)
 
-  return length
+  return string
 
 
 code_chars = 0
@@ -48,11 +39,10 @@ with open('input', 'r') as f:
     if line.endswith('\n'):
       line = line[:-1]
 
-    cur_code = count_code_chars(line)
-    cur_string = count_string_chars(line)
+    decoded_string = decode(line)
 
-    code_chars = code_chars + cur_code
-    string_chars = string_chars + cur_string
+    code_chars = code_chars + len(line)
+    string_chars = string_chars + len(decoded_string)
 
 print 'Code chars: {}'.format(code_chars)
 print 'String chars: {}'.format(string_chars)
