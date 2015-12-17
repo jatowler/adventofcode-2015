e4675a9256230fc234aec387998457f0124c30fc
Jerry Towler <jatowler@gmail.com>
2015-12-17 00:31:59 -0600

    Solve day 17, part 1

---
+++ 17/part1.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import itertools
+
+def fill_containers(amount, containers):
+  # If we have no containers left, we fail
+  if len(containers) == 0:
+    return None
+
+  # If our smallest container is too big, we fail
+  if containers[-1] > amount:
+    return None
+
+  # If all the remaining containers couldn't hold it, we fail
+  # This case will actually be handled correctly, but we can
+  # catch it explicitly and not worry about it
+  if amount > sum(containers):
+    return None
+
+  # For every container size left, use it and try to use the rest
+  combos = []
+  for i, c in enumerate(containers):
+    if amount == c:
+      # Simply use this container and we're done
+      combos.append([c])
+      continue
+
+    if amount > c:
+      # Use this container and fit the remaining liquid into the
+      # remaining containers
+      new_amount = amount - c
+      available_containers = containers[(i + 1):]
+
+      result = fill_containers(new_amount, available_containers)
+
+      if result is None:
+        # It can't be done, so add no combos
+        continue
+
+      for r in result:
+        if type(r) is int:
+          combo = [c,r]
+        else:
+          combo = [c] + r
+        combos.append(combo)
+
+  return combos
+
+eggnog_amount = 150
+containers = []
+with open('input', 'r') as f:
+  for line in f:
+    containers.append(int(line))
+
+containers = list(reversed(sorted(containers)))
+
+# The dumb way
+combinations = []
+for i in xrange(len(containers)):
+  combinations.extend(itertools.combinations(containers, i))
+
+used_combinations = []
+for c in combinations:
+  if sum(c) == eggnog_amount:
+    used_combinations.append(c)
+
+print 'There are {} available combos (dumb)'.format(len(used_combinations))
+
+# The smart way
+combos = fill_containers(eggnog_amount, containers)
+print 'There are {} available combos (smart):'.format(len(combos))
